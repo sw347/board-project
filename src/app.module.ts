d76ffbca97a02b48757c731129c693b365e90234
sw347 <>
2025-03-27 20:30:31 +0900

    reset

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,21 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardModule } from './board/board.module';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import {join} from 'path';
+import * as process from 'process';
+
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), '..','public'),
+      serveRoot: '/static'
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), '..','dist'),
+      serveRoot: '/dist'
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env'
