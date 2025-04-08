513964fc690d8e870ea53a33dbed2710de47a12d
sw347 <>
2025-04-08 14:32:48 +0900

    cors error fix

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: true,
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-    credentials: true,
+    allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
+    preflightContinue: false,
+    optionsSuccessStatus: 204
   });
+
   await app.listen(process.env.PORT || 5161);
 }
 
