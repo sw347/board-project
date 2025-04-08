54bc76fdb95df2d45dd6c2278cc1654b89ec85db
sw347 <>
2025-04-08 17:03:10 +0900

    수정2

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,12 @@ import { AppModule } from './app.module';
 import * as process from 'process';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://127.0.0.1:5500', // 정확한 클라이언트 주소
-    methods: 'GET,POST,OPTIONS', // 허용 메서드 명시
-    allowedHeaders: 'Content-Type, Authorization',
-    credentials: false // 토큰 미사용 시 false 권장[3][8]
+    origin: '*', // 또는 'http://127.0.0.1:5500' 같은 정확한 주소
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
   });
 
   await app.listen(process.env.PORT || 5161);
