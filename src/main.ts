067897271868af2d7a438ca66e126ae2e012f409
sw347 <>
2025-04-14 08:53:54 +0900

    cors method option allow

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: ['http://localhost:5500', "http://127.0.0.1:5500"], // 프론트 주소 넣기
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE, OPTION',
     credentials: true,
   });
   await app.listen(process.env.PORT || 5161);
