fa94e7ebfc17566b152bde7669b7a7a0a9b96114
sw347 <>
2025-04-14 09:09:21 +0900

    fix : cors setting

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ async function bootstrap() {
   app.enableCors({
     origin: ['http://localhost:5500', "http://127.0.0.1:5500", "https://sw347.github.io"], // 프론트 주소 넣기
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE, OPTION',
-    credentials: true,
-    maxAge: 86400,
+    allowedHeaders: ['Content-Type'],
+    credentials: false,
   });
   await app.listen(process.env.PORT || 5161);
 }
