1376a7f77c44fa9d19fa9b84bf852170914282bc
sw347 <>
2025-04-14 09:03:49 +0900

    app cors origin add

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5500', "http://127.0.0.1:5500"], // 프론트 주소 넣기
+    origin: ['http://localhost:5500', "http://127.0.0.1:5500", "https://sw347.github.io"], // 프론트 주소 넣기
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE, OPTION',
     credentials: true,
+    maxAge: 86400,
   });
   await app.listen(process.env.PORT || 5161);
 }
