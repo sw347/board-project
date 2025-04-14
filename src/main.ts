8395b3d5330512207ba1eb2f069b0e454fd2e058
sw347 <>
2025-04-14 09:16:48 +0900

    error fix

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5500', "http://127.0.0.1:5500", "https://sw347.github.io"], // 프론트 주소 넣기
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE, OPTION',
-    allowedHeaders: ['Content-Type'],
-    credentials: false,
+    origin: ['http://localhost:5500', "http://127.0.0.1:5500"], // 프론트 주소 넣기
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
   await app.listen(process.env.PORT || 5161);
 }
